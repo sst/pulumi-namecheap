ebdf5c79bb37b5d7f186b5698a5843bda46e3f0b
Frank <frank@sst.dev>
2024-04-13 05:25:15 +0000

    Sync

--- provider/resources.go
+++ provider/resources.go
@@ -121,6 +121,7 @@ func Provider() tfbridge.ProviderInfo {
 			// "aws_ami": {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAmi")},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@sst-provider/namecheap",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
